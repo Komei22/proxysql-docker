cae50ed212521e2e0be72c9029fb187c2d991092
Komei Nomura <komei.nomura@pepabo.com>
2018-07-17 17:05:46 +0900

    add shellscript to set mysql user information

--- proxysql-entry.sh
+++ proxysql-entry.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-if [ -z "$MYSQL_HOSTNAME" ]; then
-  echo >&2 'Error: You need to specify MYSQL_HOSTNAME'
+if [ -z "$MYSQL_HOST" ]; then
+  echo >&2 'Error: You need to specify $MYSQL_HOST'
+  exit 1
+fi
+
+if [ -z "$MYSQL_USER" ]; then
+  echo >&2 'Error: You need to specify $MYSQL_USER'
+  exit 1
+fi
+
+if [ -z "$MYSQL_USER_PASSWORD" ]; then
+  echo >&2 'Error: You need to specify $MYSQL_USER_PASSWORD'
   exit 1
 fi
 
--- set_mysql_info.sh
+++ set_mysql_info.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "set_mysql_info.sh"
-mysql -uadmin -padmin -h 127.0.0.1 -P6032 -e "INSERT INTO mysql_servers (hostgroup_id, hostname, port) VALUES (0, '$MYSQL_HOSTNAME', 3306)"
-mysql -h 127.0.0.1 -P6032 -uadmin -padmin -e "LOAD MYSQL SERVERS TO RUNTIME; SAVE MYSQL SERVERS TO DISK;"
+mysql -uadmin -padmin -h 127.0.0.1 -P6032 -e "INSERT INTO mysql_servers (hostgroup_id, hostname, port) VALUES (0, '$MYSQL_HOST', 3306);"
+mysql -uadmin -padmin -h 127.0.0.1 -P6032 -e "INSERT INTO mysql_users (username, password, active, default_hostgroup) VALUES ('$MYSQL_USER', '$MYSQL_USER_PASSWORD', 1, 0);"
+mysql -h 127.0.0.1 -P6032 -uadmin -padmin -e "LOAD MYSQL SERVERS TO RUNTIME; SAVE MYSQL SERVERS TO DISK; LOAD MYSQL USERS TO RUNTIME; SAVE MYSQL USERS TO DISK;"
